680e4bc0ddcd5e98f01b37aa74f12d352716b309
Daniel Ramirez <drecheverri@hotmail.com>
2024-07-03 12:01:45 -0500

    OAuth2 client configuration

---
+++ src/main/java/dev/dramireze/oauth2_client_app/Oauth2ClientAppApplication.java
@@ -0,0 +1,13 @@
+package dev.dramireze.oauth2_client_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ClientAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2ClientAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/dramireze/oauth2_client_app/Oauth2ClientAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dramireze.oauth2_client_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ClientAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
